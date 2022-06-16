971a67beb7d8cf46893b54d6c2a24e3b80de2efc
nishuang83 <nishuang83@gmail.com>
2022-06-16 13:16:13 -0400

    update june 16

--- Setup_Narval.sh
+++ Setup_Narval.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Paths
-ENV_LOC="/home/shuangni/AlzheimerProejct/Alz_env"
-
-PROJECT_DIR="/home/shuangni/AlzheimerProejct"
-
+PROJECT_DIR="/home/shuangni/AlzheimerProject"
+ENV_LOC="/home/shuangni/AlzheimerProject/alz_env"
 module load python/3.7
-#virtualenv --no-download $ENV_LOC
+# virtualenv --no-download $ENV_LOC
 source $ENV_LOC/bin/activate
 pip install --no-index --upgrade pip
-
-#salloc --time=2:0:0 --account=rrg-bengioy-ad
+pip install --no-index -r /home/shuangni/AlzheimerProject/requirements.txt
+export PYTHONPATH="$PROJECT_DIR:$PYTHONPATH"
+#salloc --time=1:0:0 --account=rrg-bengioy-ad --mem=100Gb
+# rm -rf $ENVDIR
--- test.py
+++ test.py
@@ -1,8 +1,31 @@
 import scanpy as sc
-import anndata
 import pandas as pd
+# import numpy as np
+# import sklearn
+# import phate
+import matplotlib.pyplot as plt
+ 
+PROJECT_DIR = "/home/shuangni/AlzheimerProject"
+# DATA_DIR_raw = PROJECT_DIR + '/data/msPHATE_data_raw.h5ad'
+# data_raw = sc.read_h5ad(DATA_DIR_raw)
+# print(data_raw)
+# AnnData object with n_obs × n_vars = 82159 × 32738
+# obs: 'id', 'amyloid', 'plaq_n', 'nft', 'tangles', 'cogn_global_lv', 'gpath', 'gpath_3neocort', 'amyloid.group', 'caa_4gp', 'ceradsc', 'braaksc', 'niareagansc', 'cogdx', 'msex', 'pathology.group', 'diagnosis'
+# var: 'gene_id'
 
-PROJECT_DIR = "/home/shuangni/AlzheimerProejct"
-DATA_DIR = PROJECT_DIR + '/data/msPHATE_data_pp.h5ad'
+DATA_DIR_pp = PROJECT_DIR + '/data/msPHATE_data_pp.h5ad'
+data_pp = sc.read(DATA_DIR_pp)
+print(data_pp.obs.tsne1)
+# AnnData object with n_obs × n_vars = 70634 × 17926
+# obs: 'id', 'tsne1', 'tsne2', 'pre.cluster', 'celltype', 'Subcluster', 'apoe_genotype', 'braak_stage', 'cerad_score', 'age_death', 'sex', 'pmi', 'concensus_diagnosis', 'diagnosis_cerad', 'diagnosis', 'n_genes_by_counts', 
+# 'total_counts'
+# var: 'n_cells_by_counts', 'mean_counts', 'pct_dropout_by_counts', 'total_counts'
 
-data = anndata.read(DATA_DIR)
+######
+# tsne results
+######
+tsne1 = data_pp.obs.tsne1
+tsne2 = data_pp.obs.tsne2
+
+plt.scatter(tsne1, tsne2)
+plt.show()
