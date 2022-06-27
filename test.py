4dadc0327ebb1d9e3a28a89af06ba2ee820bd4c9
nishuang83 <nishuang83@gmail.com>
2022-06-27 16:51:39 -0400

    update

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ import phate
 import multiscale_phate as MSphate
 
 PROJECT_DIR="/home/shuangni/AlzheimerProject"
-DATA_DIR_raw = PROJECT_DIR + '/data/msPHATE_data_raw.h5ad'
-data_raw = sc.read_h5ad(DATA_DIR_raw)
+# DATA_DIR_raw = PROJECT_DIR + '/data/msPHATE_data_raw.h5ad'
+# data_raw = sc.read_h5ad(DATA_DIR_raw)
 
-# DATA_DIR_pp = PROJECT_DIR + '/data/msPHATE_data_pp.h5ad'
-# data_pp = sc.read(DATA_DIR_pp)
+DATA_DIR_pp = PROJECT_DIR + '/data/msPHATE_data_pp.h5ad'
+data_pp = sc.read(DATA_DIR_pp)
 
 ######
 # PHATE
@@ -27,23 +27,24 @@ data_raw = sc.read_h5ad(DATA_DIR_raw)
 # Y_MSphate_pp = MSphate_operator.fit(data_pp)
 # Y_MSphate_pp = Y_MSphate_pp.transform(visualization_level=self.algo_obj.levels[0], **opt_kwargs)
 
-#raw data
-MSphate_operator =  MSphate.Multiscale_PHATE(random_state=1)
-levels = MSphate_operator.fit(data_raw)
+# MSphate_operator =  MSphate.Multiscale_PHATE(random_state=1)
+# levels = MSphate_operator.fit(data_raw)
 
-ax = plt.plot(MSphate_operator.gradient)
-ax = plt.scatter(levels, MSphate_operator.gradient[levels], c = 'r', s=100)
-plt.show()
+######
+# MSphate for data_pp
+######
+mp_op =  MSphate.Multiscale_PHATE(random_state=1)
+levels = mp_op.fit(data_pp)
+
+plt.figure()
+plt.plot(mp_op.gradient)
+plt.scatter(levels, mp_op.gradient[levels], c = 'r', s=100)
+plt.savefig(PROJECT_DIR + '/results/'+'levels.jpg')
 print(levels)
-######
-# plot
-######
-# ids = data_pp.obs.id
-# celltypes = data_pp.obs.celltype
-# Subclusters = data_pp.obs.Subcluster
-# cerad_scores = data_pp.obs.cerad_score
-# diagnosises = data_pp.obs.diagnosis
-# braak_stages = data_pp.obs.braak_stage
-# apoe_genotypes = data_pp.obs.apoe_genotype
+embedding, clusters, sizes = mp_op.transform(visualization_level = levels[2],cluster_level = levels[1])
+
+with open('embedding.txt','w') as f:
+    f.writelines(embedding)
+
 
 
